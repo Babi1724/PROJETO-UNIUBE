acd490935987fa45c69348d27d936c49d94f24ac
Barbara Cristina <102261496+Babi1724@users.noreply.github.com>
2025-03-06 20:29:51 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,14 @@
+<html>
+    <title>Primeira Aula</title>
+    <head></head>
+
+    <body>
+        <form method="post" action="teste.php">
+            Nome: <input type="text" name="nome"><br>
+            <br>
+            Idade: <input type="text" name="idade"><br>
+            <br>
+            <input type="submit" value="enviar">
+        </form>
+    </body>
+</html>
---
+++ Teste.php
@@ -0,0 +1,8 @@
+<?
+    echo "nome: ".$_POST['Nome'];
+    echo '<br';
+    echo "Idade: ".$_POST['Idade'];
+    echo '<hr';
+
+    print(.$POST['Nome']);
+?>
