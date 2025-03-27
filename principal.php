2be10132bf24b9ba026a9823f6c207fce1425c04
Barbara Cristina <102261496+Babi1724@users.noreply.github.com>
2025-03-27 20:08:40 -0300

    criacao do layout do site

--- principal.php
+++ principal.php
@@ -1,9 +1,39 @@
-<?
+#<?
 
-include("autenticacao.php");
+#include("autenticacao.php");
 
-echo "cpf:" .$_SESSION['cpf'].'<br>';
-echo "nome: " . $_SESSION['nome'] '<br>';
-echo "senha: " . $_SESSION['senha'];
+#echo "cpf:" .$_SESSION['cpf'].'<br>';
+#echo "nome: " . $_SESSION['nome'] '<br>';
+#echo "senha: " . $_SESSION['senha'];
 
-?>
+#?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de filmes</title>
+</head>
+<body>
+    <div style= "width:800px;margin:0 auto;">
+        <div style="min-height:100px; width:100%;background-color:#4caf50;";>
+            <div style="width:50%;float:left">
+            <span style="padding-left:10px;">Ola<?=$_SESSION['nome'];?></span>
+            </div>
+
+            <div style = "width: 50%; float:left; text-align:right;">
+            <span style="background-color:blue;margin-right:10px;"><ahref="sair.php"><font color="black">
+            </div>
+        </div>
+        <div id="menu" style="width:200px;background-color:#f4f4f4;min-height:400px;float:left;">
+            <h2>Menu</h2>
+            <p><a href="item.php"><font color="black">item 1 </font></a></p>
+            <p>Item 2 </p>
+            <p>Item 3 </p>
+    </div>
+
+    <div style= "background-color: #ddd;min-height:400px;width:600px;float:left">
+        <h2>Manutenção de filmes</h2>
+</body>
+</html>
